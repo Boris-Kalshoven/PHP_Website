0a7ccdbdd52e3be7b1acb965171ef3d86a342e80
Boris Kalshoven <boris.kalshoven@gmail.com>
2021-11-12 00:23:24 +0100

    Fixed Login System

--- authentication.php
+++ authentication.php
@@ -11,9 +11,9 @@ $password = stripcslashes($password);
 $username = mysqli_real_escape_string($conn, $username);
 $password = mysqli_real_escape_string($conn, $password);
 
-$sql = "SELECT * FROM account where username='$username' and password='$password'";
+$sql = "SELECT * FROM 'users' where username='$username' and password='$password'";
 $result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_array($result);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 $count = mysqli_num_rows($result);
 
 if ($count == 2) {
